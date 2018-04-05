27b3fb62e9c85d91280c51f1bfe748022ba7527b
Pravin Pareekh <pravin.pareekh@synerzip.com>
2018-04-05 22:05:57 +0530

    lib for Subscription type

--- lib/util.d.ts
+++ lib/util.d.ts
@@ -1,2 +1,3 @@
 import { GraphQLFieldMap, GraphQLFieldConfigMap } from 'graphql';
+export declare function fieldMapToFieldConfigMapForsubscribe(fields: GraphQLFieldMap<any, any>): GraphQLFieldConfigMap<any, any>;
 export declare function fieldMapToFieldConfigMap(fields: GraphQLFieldMap<any, any>): GraphQLFieldConfigMap<any, any>;
